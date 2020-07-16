7e29044c36ae44e0f241f7ed6b9b7e756815409a
webDrag0n <linzexinmasterchief@gmail.com>
2020-07-16 04:04:42 +0100

    function test

--- src/Hello.scala
+++ src/Hello.scala
@@ -1,3 +1,14 @@
 object Hello extends App {
-  println("wiiiii!!! Hello, World!")
+  var boo = true
+  var text = "aaaaaaaaaa!!!"
+
+  if (!boo) println("wiiiii!!! Hello, World!" + text)
+  if (boo) println(text)
+
+  var x = for (i <- 1 to 5) yield i * 2
+  for (i <- x) println(i)
+
+  var h = new HelloWorld()
+  var s = h.sayHi()
+  println(s)
 }
--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -1,3 +1,3 @@
 class HelloWorld {
-
+  def sayHi(): String = "hi main"
 }
